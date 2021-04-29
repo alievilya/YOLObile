963b0a48d23784daf0491bd75eb9d4ba119c3b36
Aliev Ilya <aliev.ilia@yandex.ru>
2021-04-29 14:55:15 +0300

    detection with tensorrt for yolov4 named det_yolov4trt.py

--- det_yolov4trt.py
+++ det_yolov4trt.py
@@ -1,4 +1,10 @@
 import socket
+import os
+import time
+import argparse
+import cv2
+import pycuda.autoinit  # This is needed for initializing CUDA driver
+
 
 from deep_sort_pytorch.deep_sort import DeepSort
 from deep_sort_pytorch.utils.parser import get_config
@@ -7,6 +13,78 @@ from tracking_modules import Counter, Writer
 from tracking_modules import find_centroid, Rectangle, rect_square, bbox_rel, draw_boxes, select_object
 from utils.datasets import *
 from utils.utils import *
+
+from utils.yolo_classes import get_cls_dict
+from utils.visualization import BBoxVisualization
+from utils.yolo_with_plugins import get_input_shape, TrtYOLO
+
+
+WINDOW_NAME = 'TrtYOLODemo'
+
+
+# def parse_args():
+#     """Parse input arguments."""
+#     desc = ('Run the TensorRT optimized object detecion model on an input '
+#             'video and save BBoxed overlaid output as another video.')
+#     parser = argparse.ArgumentParser(description=desc)
+#     parser.add_argument(
+#         '-v', '--video', type=str, required=True,
+#         help='input video file name')
+#     parser.add_argument(
+#         '-o', '--output', type=str, required=True,
+#         help='output video file name')
+#     parser.add_argument(
+#         '-c', '--category_num', type=int, default=80,
+#         help='number of object categories [80]')
+#     parser.add_argument(
+#         '-m', '--model', type=str, required=True,
+#         help=('[yolov3|yolov3-tiny|yolov3-spp|yolov4|yolov4-tiny]-'
+#               '[{dimension}], where dimension could be a single '
+#               'number (e.g. 288, 416, 608) or WxH (e.g. 416x256)'))
+#     parser.add_argument(
+#         '-l', '--letter_box', action='store_true',
+#         help='inference with letterboxed image [False]')
+#     args = parser.parse_args()
+#     return args
+
+
+def perform_detection(frame, trt_yolo, conf_th, vis):
+    """Continuously capture images from camera and do object detection.
+
+    # Arguments
+      cap: the camera instance (video source).
+      trt_yolo: the TRT YOLO object detector instance.
+      conf_th: confidence/score threshold for object detection.
+      vis: for visualization.
+      writer: the VideoWriter object for the output video.
+    """
+
+    if frame is None: print('no frame')
+    boxes, confs, clss = trt_yolo.detect(frame, conf_th)
+    frame = vis.draw_bboxes(frame, boxes, confs, clss)
+    return boxes, confs, clss
+
+def run_detection():
+    # args = parse_args()
+    with open("cfg/detection_tracker_cfg.json") as detection_config:
+        detect_config = json.load(detection_config)
+
+    if detect_config["category_num"] <= 0:
+        raise SystemExit('ERROR: bad category_num (%d)!' % detect_config["category_num"])
+    if not os.path.isfile('yolo/%s.trt' % detect_config["model"]):
+        raise SystemExit('ERROR: file (yolo/%s.trt) not found!' % detect_config["model"])
+
+    cap = cv2.VideoCapture(detect_config["source"])
+    if not cap.isOpened():
+        raise SystemExit('ERROR: failed to open the input video file!')
+    frame_width, frame_height = int(cap.get(3)), int(cap.get(4))
+
+    cls_dict = get_cls_dict(detect_config["category_num"])
+    vis = BBoxVisualization(cls_dict)
+    h, w = get_input_shape(detect_config["model"])
+    trt_yolo = TrtYOLO(detect_config["model"], (h, w), detect_config["category_num"], detect_config["letter_box"])
+    ret, frame = cap.read()
+    boxes, confs, clss = perform_detection(frame=frame, trt_yolo=trt_yolo, conf_th=0.3, vis=vis)
 
 
 def detect(config):
@@ -51,191 +129,136 @@ def detect(config):
                         nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
                         max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
                         use_cuda=True)
-
     # Initialize device, weights etc.
-    device = torch_utils.select_device(device='cpu' if ONNX_EXPORT else config["device"])
     if os.path.exists(out):
         shutil.rmtree(out)  # delete output folder
     os.makedirs(out)  # make new output folder
-    half = device.type != 'cpu'  # half precision only supported on CUDA
-    # Initialize model
-    model = Darknet(config["cfg"], imgsz)
-
-    # Load weights
-    attempt_download(weights)
-    if weights.endswith('.pt'):  # pytorch format
-        model.load_state_dict(torch.load(weights, map_location=device)['model'], strict=False)
-    else:  # darknet format
-        load_darknet_weights(model, weights)
-    # Eval mode
-    model.to(device).eval()
-    # Half precision
-    half = half and device.type != 'cpu'  # half precision only supported on CUDA
-    if half:
-        model.half()
-
-    if webcam:
-        view_img = True
-        torch.backends.cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz)
-    else:
-        save_img = True
-        view_img = True
-        dataset = LoadImages(source, img_size=imgsz)
-
-    # Get names and colors
+    # Initialize colors
     names = load_classes(config["names"])
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
 
-    img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
-    _ = model(img.half() if half else img.float()) if device.type != 'cpu' else None  # run once
+    if config["category_num"] <= 0:
+        raise SystemExit('ERROR: bad category_num (%d)!' % config["category_num"])
+    if not os.path.isfile('yolo/%s.trt' % config["model"]):
+        raise SystemExit('ERROR: file (yolo/%s.trt) not found!' % config["model"])
+
+    cap = cv2.VideoCapture(config["source"])
+    if not cap.isOpened():
+        raise SystemExit('ERROR: failed to open the input video file!')
+    frame_width, frame_height = int(cap.get(3)), int(cap.get(4))
+
+    cls_dict = get_cls_dict(config["category_num"])
+    vis = BBoxVisualization(cls_dict)
+    h, w = get_input_shape(config["model"])
+    trt_yolo = TrtYOLO(config["model"], (h, w), config["category_num"], config["letter_box"])
+
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((HOST, PORT))
-        for frame_idx, (path, img, im0s, vid_cap) in enumerate(dataset):
+        # for frame_idx, (path, img, im0s, vid_cap) in enumerate(dataset):
+        while True:
+            ret, im0 = cap.read()
+            preds, confs, clss = perform_detection(frame=im0, trt_yolo=trt_yolo, conf_th=config["conf_thres"], vis=vis)
+
             flag_move = False
             flag_anyone_in_door = False
-
-            # if motion_detection:
-            #
-            #     if webcam:  # batch_size >= 1
-            #         p, s, im0 = path[0], '%g: ' % i, im0s[0].copy()
-            #     else:
-            #         p, s, im0 = path, '', im0s
-            #
-            #     flag_move = MoveDetector.find_motion(im0)
-            #
-            # #  TODO motion detection instead of yolo
-            # if flag_move:
-            #     print("flag_move = True")
-            # else:
-            #     continue
-
-
-            t0_ds = time.time()
+            t0 = time.time()
             ratio_detection = 0
-            img = torch.from_numpy(img).to(device)
-            img = img.half() if half else img.float()  # uint8 to fp16/32
-            img /= 255.0  # 0 - 255 to 0.0 - 1.0
-            if img.ndimension() == 3:
-                img = img.unsqueeze(0)
-            # Inference
-            t1 = torch_utils.time_synchronized()
-            pred = model(img, augment=config["augment"])[0]
 
-            # to float
-            if half:
-                pred = pred.float()
-            # Apply NMS
-            classes = None if config["classes"] == "None" else config["classes"]
-            pred = non_max_suppression(pred, config["conf_thres"], config["iou_thres"],
-                                       multi_label=False, classes=classes, agnostic=config["agnostic_nms"])
             # Process detections
             lost_ids = counter.return_lost_ids()
-            for i, det in enumerate(pred):  # detections for image i
-                if webcam:  # batch_size >= 1
-                    p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
-                else:
-                    p, s, im0 = path, '', im0s
-
-                if len(door_array) != 4 or len(around_door_array) != 4:
-                    door_array = select_object(im0)
-                    print(door_array)
-
-                save_path = str(Path(out) / Path(p).name)
-                s += '%gx%g ' % img.shape[2:]  # print string
-                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  #  normalization gain whwh
-                # lost_ids = counter.return_lost_ids()
-
+            for i, det in enumerate(preds):  # detections for image i
+                # if webcam:  # batch_size >= 1
+                #     p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
+                # else:
+                #     p, s, im0 = path, '', im0s
                 if det is not None and len(det):
                     # Rescale boxes from imgsz to im0 size
-                    det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                    # det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
                     # Print results
                     for c in det[:, -1].unique():
                         if names[int(c)] not in config["needed_classes"]:
                             continue
-                        n = (det[:, -1] == c).sum()  # detections per class
-                        s += '%g %s, ' % (n, names[int(c)])  # add to string
-                    bbox_xywh = []
-                    confs = []
+                    # bbox_xywh = []
+                    # confs = []
                     # Write results
                     for *xyxy, conf, cls in det:
                         #  check if bbox`s class is needed
                         if names[int(cls)] not in config["needed_classes"]:
                             continue
-                        x_c, y_c, bbox_w, bbox_h = bbox_rel(*xyxy)
-                        obj = [x_c, y_c, bbox_w, bbox_h]
-                        bbox_xywh.append(obj)
-                        confs.append([conf.item()])
+                        # x_c, y_c, bbox_w, bbox_h = bbox_rel(*xyxy)
+                        # obj = [x_c, y_c, bbox_w, bbox_h]
+                        # bbox_xywh.append(obj)
+                        # confs.append([conf.item()])
 
                         if save_img or view_img:  # Add bbox to image
                             label = '%s %.2f' % (names[int(cls)], conf)
                             plot_one_box(xyxy, im0, label=label, color=colors[int(cls)])
 
-                    detections = torch.Tensor(bbox_xywh)
-                    confidences = torch.Tensor(confs)
+            detections = torch.Tensor(preds)
+            confidences = torch.Tensor(confs)
 
-                    # Pass detections to deepsort
-                    if len(detections) == 0:
-                        continue
-                    outputs = deepsort.update(detections, confidences, im0)
+            # Pass detections to deepsort
+            if len(detections) == 0:
+                continue
+            outputs = deepsort.update(detections, confidences, im0)
 
-                    # draw boxes for visualization
-                    if len(outputs) > 0:
-                        bbox_xyxy = outputs[:, :4]
-                        identities = outputs[:, -1]
-                        draw_boxes(im0, bbox_xyxy, identities)
-                        counter.update_identities(identities)
+            # draw boxes for visualization
+            if len(outputs) > 0:
+                bbox_xyxy = outputs[:, :4]
+                identities = outputs[:, -1]
+                draw_boxes(im0, bbox_xyxy, identities)
+                counter.update_identities(identities)
 
-                        for bbox_tracked, id_tracked in zip(bbox_xyxy, identities):
+                for bbox_tracked, id_tracked in zip(bbox_xyxy, identities):
 
-                            rect_detection = Rectangle(bbox_tracked[0], bbox_tracked[1],
-                                                       bbox_tracked[2], bbox_tracked[3])
-                            inter_detection = rect_detection & rect_around_door
-                            if inter_detection:
-                                inter_square_detection = rect_square(*inter_detection)
-                                cur_square_detection = rect_square(*rect_detection)
-                                try:
-                                    ratio_detection = inter_square_detection / cur_square_detection
-                                except ZeroDivisionError:
-                                    ratio_detection = 0
-                                #  чел первый раз в контуре двери
-                            if ratio_detection > 0.2:
-                                if VideoHandler.counter_frames_indoor == 0:
-                                    #     флаг о начале записи
-                                    VideoHandler.start_video(id_tracked)
-                                flag_anyone_in_door = True
+                    rect_detection = Rectangle(bbox_tracked[0], bbox_tracked[1],
+                                               bbox_tracked[2], bbox_tracked[3])
+                    inter_detection = rect_detection & rect_around_door
+                    if inter_detection:
+                        inter_square_detection = rect_square(*inter_detection)
+                        cur_square_detection = rect_square(*rect_detection)
+                        try:
+                            ratio_detection = inter_square_detection / cur_square_detection
+                        except ZeroDivisionError:
+                            ratio_detection = 0
+                        #  чел первый раз в контуре двери
+                    if ratio_detection > 0.2:
+                        if VideoHandler.counter_frames_indoor == 0:
+                            #     флаг о начале записи
+                            VideoHandler.start_video(id_tracked)
+                        flag_anyone_in_door = True
 
-                            elif ratio_detection > 0.2 and id_tracked not in VideoHandler.id_inside_door_detected:
-                                VideoHandler.continue_opened_video(id=id_tracked, seconds=3)
-                                flag_anyone_in_door = True
+                    elif ratio_detection > 0.2 and id_tracked not in VideoHandler.id_inside_door_detected:
+                        VideoHandler.continue_opened_video(id=id_tracked, seconds=3)
+                        flag_anyone_in_door = True
 
-                            # elif ratio_detection > 0.6 and counter.people_init.get(id_tracked) == 1:
-                            #     VideoHandler.continue_opened_video(id=id_tracked, seconds=0.005)
+                    # elif ratio_detection > 0.6 and counter.people_init.get(id_tracked) == 1:
+                    #     VideoHandler.continue_opened_video(id=id_tracked, seconds=0.005)
 
-                            if id_tracked not in counter.people_init or counter.people_init[id_tracked] == 0:
-                                counter.obj_initialized(id_tracked)
-                                rect_head = Rectangle(bbox_tracked[0], bbox_tracked[1], bbox_tracked[2],
-                                                      bbox_tracked[3])
-                                intersection = rect_head & rect_door
-                                if intersection:
-                                    intersection_square = rect_square(*intersection)
-                                    head_square = rect_square(*rect_head)
-                                    rat = intersection_square / head_square
-                                    if rat >= 0.4 and bbox_tracked[3] > low_border :
-                                        #     was initialized in door, probably going out of office
-                                        counter.people_init[id_tracked] = 2
-                                    elif rat < 0.4:
-                                        #     initialized in the corridor, mb going in
-                                        counter.people_init[id_tracked] = 1
-                                else:
-                                    # res is None, means that object is not in door contour
-                                    counter.people_init[id_tracked] = 1
-                                counter.frame_age_counter[id_tracked] = 0
+                    if id_tracked not in counter.people_init or counter.people_init[id_tracked] == 0:
+                        counter.obj_initialized(id_tracked)
+                        rect_head = Rectangle(bbox_tracked[0], bbox_tracked[1], bbox_tracked[2],
+                                              bbox_tracked[3])
+                        intersection = rect_head & rect_door
+                        if intersection:
+                            intersection_square = rect_square(*intersection)
+                            head_square = rect_square(*rect_head)
+                            rat = intersection_square / head_square
+                            if rat >= 0.4 and bbox_tracked[3] > low_border :
+                                #     was initialized in door, probably going out of office
+                                counter.people_init[id_tracked] = 2
+                            elif rat < 0.4:
+                                #     initialized in the corridor, mb going in
+                                counter.people_init[id_tracked] = 1
+                        else:
+                            # res is None, means that object is not in door contour
+                            counter.people_init[id_tracked] = 1
+                        counter.frame_age_counter[id_tracked] = 0
 
-                                counter.people_bbox[id_tracked] = bbox_tracked
+                        counter.people_bbox[id_tracked] = bbox_tracked
 
-                            counter.cur_bbox[id_tracked] = bbox_tracked
+                    counter.cur_bbox[id_tracked] = bbox_tracked
                 else:
                     deepsort.increment_ages()
                 # Print time (inference + NMS)
@@ -348,11 +371,11 @@ def detect(config):
                 VideoHandler.continue_writing(im0, flag_anyone_in_door)
 
             if view_img:
-                cv2.imshow(p, im0)
+                cv2.imshow('image', im0)
                 if cv2.waitKey(1) == ord('q'):  # q to quit
                     raise StopIteration
 
-            delta_time = (torch_utils.time_synchronized() - t1)
+            delta_time = (time.time() - t0)
             # t2_ds = time.time()
             # print('%s Torch:. (%.3fs)' % (s, t2 - t1))
             # print('Full pipe. (%.3fs)' % (t2_ds - t0_ds))
