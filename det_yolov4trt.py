1310151fa765c3a3aea71b5aa61c33529b61cc1a
alievilya <aliev.ilia@yandex.ru>
2021-05-17 17:58:28 +0300

    fix

--- det_yolov4trt.py
+++ det_yolov4trt.py
@@ -353,6 +353,12 @@ def detect(config):
                 send_message_daily(current_date=date, counter_in=in_counted, counter_out=out_counted)
                 counter = Counter(0, 0, 0)
                 VideoHandler = Writer()
+                deepsort = DeepSort(cfg.DEEPSORT.REID_CKPT,
+                        max_dist=cfg.DEEPSORT.MAX_DIST, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
+                        nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
+                        max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
+                        use_cuda=True)
+
                 VideoHandler.set_fps(fps)
 
 # python detect.py --cfg cfg/csdarknet53s-panet-spp.cfg --weights cfg/best14x-49.pt --source 0
